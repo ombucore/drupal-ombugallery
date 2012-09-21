332c875a7a116e48a769f5c8d53d88177cf72f31
Martin Rio <martin@ombuweb.com>
2012-09-21 16:28:06 -0700

    allows for "styles" (default, fullscreen)

--- includes/OmbuslideBean.php
+++ includes/OmbuslideBean.php
@@ -4,4 +4,38 @@
  * ombuslide.bean.inc
  */
 
-class OmbuslideBean extends BeanPlugin {}
+class OmbuslideBean extends BeanPlugin {
+
+  /**
+   * Implements the value() method.
+   */
+  public function values() {
+    $values = parent::values();
+    $values += array(
+      'style' => 'default',
+    );
+    return $values;
+  }
+
+  /**
+   * Implements the form() method.
+   */
+  public function form($bean, $form, &$form_state) {
+
+    $form = parent::form($bean, $form, $form_state);
+
+    $styles = array(
+      'default' => 'Default',
+      'fullscreen' => 'Full Screen',
+    );
+
+    $form['style'] = array(
+      '#title' => t('Style'),
+      '#type' => 'select',
+      '#required' => TRUE,
+      '#options' => $styles,
+      '#default_value' => $bean->style,
+    );
+    return $form;
+  }
+}
