0c9cdc5ec174118aac7aa72c1d0a3850b95ec194
Martin Rio <martin@ombuweb.com>
2012-09-27 00:16:28 -0700

    default style: adds next prev

--- styles/default/js/ombuslide.js
+++ styles/default/js/ombuslide.js
@@ -3,9 +3,14 @@
     attach: function(context, settings) {
       for (var slideshow in settings.ombuslide) {
         if ($('#' + slideshow + ' ul.slides > li').length > 1) {
-          $('#' + slideshow + ' ul.slides', context).after('<ul class="pager" id="' + slideshow + '-nav">').cycle({
+          var slides = $('#' + slideshow + ' ul.slides', context);
+          slides.after('<a href="#" class="prev">Previous</a><ul class="pager" id="' + slideshow + '-nav">');
+          $('#' + slideshow + ' ul.pager', context).after('<a href="#" class="next">Next</a>');
+          slides.cycle({
             timeout: 0,
             pager: '#' + slideshow + '-nav',
+            next: '#ombuslide .next',
+            prev: '#ombuslide .prev',
             pagerAnchorBuilder: function(index, element) {
               return '<li class="' + index + '"><a href="#">' + (index + 1) + '</a></li>';
             }
