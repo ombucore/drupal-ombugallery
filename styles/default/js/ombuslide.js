5a5864b3ee2fdb4085908a5fe68993e90c67c50b
Martin Rio <martin@ombuweb.com>
2012-09-27 18:23:20 -0700

    default style: starts implementing amma look & feel

--- styles/default/js/ombuslide.js
+++ styles/default/js/ombuslide.js
@@ -4,13 +4,16 @@
       for (var slideshow in settings.ombuslide) {
         if ($('#' + slideshow + ' ul.slides > li').length > 1) {
           var slides = $('#' + slideshow + ' ul.slides', context);
-          slides.after('<a href="#" class="prev">Previous</a><ul class="pager" id="' + slideshow + '-nav">');
-          $('#' + slideshow + ' ul.pager', context).after('<a href="#" class="next">Next</a>');
-          slides.cycle({
+          slides.after('<div class="pager-wrapper"><a href="#" class="prev">Previous</a><ul class="pager" id="' + slideshow + '-nav">');
+          $('#' + slideshow + ' ul.pager', context).after('<a href="#" class="next">Next</a></div>');
+          x = slides.cycle({
             timeout: 0,
             pager: '#' + slideshow + '-nav',
             next: '#ombuslide .next',
             prev: '#ombuslide .prev',
+            width: 'auto',
+            height: '430px',
+            speed: '300',
             pagerAnchorBuilder: function(index, element) {
               return '<li class="' + index + '"><a href="#">' + (index + 1) + '</a></li>';
             }
