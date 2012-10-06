cae5fbd3e058d3620e7f4eeb153e41d0198efaed
Martin Rio <martin@ombuweb.com>
2012-10-06 16:03:09 -0700

    adds settings form to slideshow field formatter - refs amaa #2215

--- styles/default/js/ombuslide.js
+++ styles/default/js/ombuslide.js
@@ -6,18 +6,13 @@
           var slides = $('#' + slideshow + ' ul.slides', context);
           slides.after('<div class="pager-wrapper"><div class="controls"><a href="#" class="prev">Previous</a><ul class="ombuslide-pager" id="' + slideshow + '-nav">');
           $('#' + slideshow + ' ul.ombuslide-pager', context).after('<a href="#" class="next">Next</a></div></div>');
-          x = slides.cycle({
-            timeout: 0,
-            pager: '#' + slideshow + '-nav',
-            next: '#ombuslide .next',
-            prev: '#ombuslide .prev',
-            width: 'auto',
-            height: '430px',
-            speed: '300',
-            pagerAnchorBuilder: function(index, element) {
-              return '<li class="' + index + '"><a href="#">' + (index + 1) + '</a></li>';
-            }
-          });
+          var opts = Drupal.settings.ombuslide[slideshow];
+          opts.pager = '#' + slideshow + '-nav';
+          opts.pagerAnchorBuilder = function(index, element) {
+              return '<li class="' + index + '"><a href="#">' + (index + 1) +
+              '</a></li>';
+          }
+          slides.cycle(opts).cycle();
         }
       }
     }
