b3f3537e29dc3d2422945471fa705719391c2be3
Adam Page <adamfpage@gmail.com>
2013-08-20 16:56:19 -0700

    Preliminary styles and behaviors for responsive slideshow on mobile.

--- modules/ombuslide_responsive/styles/responsive/js/ombuslide.js
+++
@@ -1,7 +0,0 @@
-(function ($) {
-  Drupal.behaviors.ombuslide = {
-    attach: function(context, settings) {
-      console.log('Responsive ombuslide in the house.')
-    }
-  }
-})(jQuery);
---
+++ modules/ombuslide_responsive/styles/responsive/js/ombuslide.responsive.js
@@ -0,0 +1,32 @@
+(function ($) {
+  Drupal.behaviors.ombuslide = {
+    attach: function(context, settings) {
+      console.log('Responsive ombuslide in the house.')
+
+      // On click handler for slide anchor links
+      $('.ombuslide-responsive .slides-nav a').on('click', function(e) {
+
+        // Get the container of this particular slideshow
+        var container = $(this).closest('.ombuslide-responsive');
+
+        // Get the index of the clicked slide in relation to its siblings
+        var index = container.find('.slides-nav li').index($(this).parent());
+
+        // Set the left position of the corresponding slide; transition rules
+        // in the CSS file will handle the animation
+        container.find('ul.slides').css('left', (index * -100) + '%');
+
+        // Set this nav link to the active state and all others to inactive
+        container.find('.slides-nav li').removeClass('active');
+        $(this).parent().addClass('active');
+
+	      e.preventDefault();
+	      e.stopPropagation();
+	      return false;
+      });
+
+      // Set the first slide link to active for each ombuslide
+      $('.ombuslide-responsive .slides-nav li:first-child').addClass('active');
+    }
+  }
+})(jQuery);
