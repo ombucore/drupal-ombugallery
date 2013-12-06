f60747fd3200415cc46ada30a7ee68d7f06af2fb
Adam Page <adamfpage@gmail.com>
2013-12-05 17:17:43 -0800

    Refs #5519: Preliminary, incomplete styles and behaviors for ombuslide’s default style.

--- styles/default/js/ombuslide.js
+++ styles/default/js/ombuslide.js
@@ -2,16 +2,25 @@
   Drupal.behaviors.ombuslide = {
     attach: function(context, settings) {
       for (var slideshow in settings.ombuslide) {
-        if ($('#' + slideshow + ' ul.slides > li').length > 1) {
-          var slides = $('#' + slideshow + ' ul.slides', context);
-          slides.after('<div class="pager-wrapper"><div class="controls"><a href="#" class="prev">Previous</a><ul class="ombuslide-pager" id="' + slideshow + '-nav">');
-          $('#' + slideshow + ' ul.ombuslide-pager', context).after('<a href="#" class="next">Next</a></div></div>');
+        if ($('#' + slideshow + ' > li').length > 1) {
+          // var $slideshow = $('#' + slideshow);
+          var slides = $('#' + slideshow, context);
+
+          console.log(slides);
+
+
+          // slides.after('<div class="pager-wrapper"><div class="controls"><a href="#" class="prev">Previous</a><ul class="ombuslide-pager" id="' + slideshow + '-nav">');
+          // $('#' + slideshow + ' ul.ombuslide-pager', context).after('<a href="#" class="next">Next</a></div></div>');
           var opts = Drupal.settings.ombuslide[slideshow];
-          opts.pager = '#' + slideshow + '-nav';
-          opts.pagerAnchorBuilder = function(index, element) {
-              return '<li class="' + index + '"><a href="#">' + (index + 1) +
-              '</a></li>';
-          }
+          // opts.pager = '#' + slideshow + '-nav';
+          // opts.pagerAnchorBuilder = function(index, element) {
+          //     return '<li class="' + index + '"><a href="#">' + (index + 1) +
+          //     '</a></li>';
+          // }
+
+
+          console.log(opts);
+
           slides.cycle(opts)
         }
       }
