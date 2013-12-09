60202fc0369be3654f8768eb826b396ea5df404d
Adam Page <adamfpage@gmail.com>
2013-12-09 15:49:23 -0800

    Refs #5522: Removed cruft.

--- styles/default/js/ombuslide.js
+++ styles/default/js/ombuslide.js
@@ -1,26 +1,16 @@
 (function ($) {
   Drupal.behaviors.ombuslide = {
     attach: function(context, settings) {
+
+      // Process each ombuslide slideshow on page.
       for (var slideshow in settings.ombuslide) {
+
+        // Only process slideshows that have one or more slides.
         if ($('#' + slideshow + ' > li').length > 1) {
-          // var $slideshow = $('#' + slideshow);
           var slides = $('#' + slideshow, context);
-
-          console.log(slides);
-
-
-          // slides.after('<div class="pager-wrapper"><div class="controls"><a href="#" class="prev">Previous</a><ul class="ombuslide-pager" id="' + slideshow + '-nav">');
-          // $('#' + slideshow + ' ul.ombuslide-pager', context).after('<a href="#" class="next">Next</a></div></div>');
           var opts = Drupal.settings.ombuslide[slideshow];
-          // opts.pager = '#' + slideshow + '-nav';
-          // opts.pagerAnchorBuilder = function(index, element) {
-          //     return '<li class="' + index + '"><a href="#">' + (index + 1) +
-          //     '</a></li>';
-          // }
 
-
-          console.log(opts);
-
+          // Instantiate jQuery Cycle 2 plugin.
           slides.cycle(opts)
         }
       }
