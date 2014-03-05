f4fdea34a66cae8fb66565666889044a406a9388
Adam Page <adamfpage@gmail.com>
2014-03-05 11:25:49 -0800

    Refs #6839: When a video exists in the slideshow, resize its iframe on window resize.

--- styles/default/js/ombuslide.js
+++ styles/default/js/ombuslide.js
@@ -19,12 +19,28 @@
           // Instantiate jQuery Cycle 2 plugin.
           $slides.cycle(opts)
 
-          // Pause the slideshow if the user hovers clicks one of its elements.
-          $slideshow.on('click', function() {
+          // Pause the slideshow if the user clicks or hovers anywhere inside
+          // its container element.
+          $slideshow.on('click mouseover', function() {
             $slides.cycle('pause');
           });
+
+          // Resize the iframe on load and when the browse window is resized.
+          $(window).on('resize', function() {
+            resizeVideo($slideshow, $slideshow.find('.file-video iframe'))
+          });
+
+          resizeVideo($slideshow, $slideshow.find('.file-video iframe'))
         }
       }
     }
+  }
+
+  function resizeVideo($container, $video) {
+    var aspectRatio = $video.attr('height') / $video.attr('width');
+    var newWidth = $container.width();
+    var newHeight = newWidth * aspectRatio;
+    $video.attr('width', newWidth);
+    $video.attr('height', newHeight);
   }
 })(jQuery);
